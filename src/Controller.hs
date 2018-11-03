135d94b7e285cc46f4be1de4df09aa10f979866e
Marieke van der Plaat <m.d.vanderplaat@students.uu.nl>
2018-11-03 08:43:08 +0100

    rotation (niet goed)

--- src/Controller.hs
+++ src/Controller.hs
@@ -25,8 +25,14 @@ input e gstate = return (inputKey e gstate)
 
 inputKey :: Event -> GameState -> GameState
 inputKey (EventKey (Char c) cs _ _) gstate = case cs of 
-                                           Down -> case c of 'w' -> gstate {player = move (player gstate)} --changePlayerInGS gstate -- If the user presses a character key, show that one
+                                           Down -> case c of 'w' -> gstate {player = move (player gstate), keyStates = changeElement 0 Down (keyStates gstate)} --changePlayerInGS gstate -- If the user presses a character key, show that one
+                                                             'a' -> gstate {player = rotation 1.0 (player gstate)} 
+                                                             'd' -> gstate {player = rotation (-1.0) (player gstate)}                                                
                                                              _  -> gstate
                                            _ -> gstate {keyStates = initialKeys }
 inputKey _ gstate = gstate  -- Otherwise keep the same
+
+changeElement :: Int -> a -> [a] -> [a]
+changeElement _ _ [] = []
+changeElement n newElement xs = take n xs ++ [newElement] ++ drop (n + 1) xs      
     
